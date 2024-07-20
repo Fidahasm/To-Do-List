273086d2af9a4a1b6d6e23718edf31ecacd80e07
Fidahasm <fidahkt@gmail.com>
2024-07-20 23:49:45 +0530

    second commit

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
+ 
+ 
+ 
  function AddTask()
     {
+        document.getElementById('select').style.display="none";
+        var taskContent = document.getElementById('task').value;
+
         if( document.getElementById("task").value.length > 0 )
             {
-                // document.getElementById("list").innerHTML = document.getElementById("task").value;
+                const clsbtn=document.createElement('button');
+                clsbtn.textContent='Close List';
+                clsbtn.id="close";
+                clsbtn.addEventListener('click',CloseList);
+                function CloseList(){
+                    const newDiv=document.createElement('div');
+                    newDiv.id="content";
+                    const newList=document.createElement('ul');
+                    newDiv.appendChild(weeks);
+                    newDiv.appendChild(days);
+                    newList.appendChild(taskItem);
+                    newDiv.appendChild(newList);
+                   
+                }
+                element=document.getElementById('close');
                 const taskItem=document.createElement('li');
-                taskItem.textContent = document.getElementById('task').value;
+                taskItem.textContent = taskContent
 
                 const dltbtn = document.createElement('button');
                 dltbtn.textContent = 'Delete';
@@ -13,14 +33,26 @@
                 });
                 taskItem.appendChild(dltbtn);
                 list.appendChild(taskItem);
-                
-                document.getElementById('task').value=" ";
+
+                if(!element){
+                    buttons.appendChild(clsbtn)
+                }
+               
+
+    
+                // list.appendChild(weeks);
+                // list.appendChild(days);
+
+                var weeks=document.getElementById('week').value;
+                var days=document.getElementById('day').value;
+               document.getElementById('head1').innerHTML=weeks;
+               document.getElementById('head2').innerHTML=days;
+                taskContent=" ";
                 
 
             }
             else{
 
-                //var task1=document.getElementById('task').value;
                alert("Add a task!!");
 
             }
